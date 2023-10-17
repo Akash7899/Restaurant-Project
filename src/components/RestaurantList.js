902193de495c7d5dc192570826533598cc2f72cc
Akash7899 <aka7899akash@gmail.com>
2023-10-17 23:56:45 +0530

    api call and test api

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const RestaurantList = () => {
-  return (
-    <h1>RestaurantList</h1>
-  )
+export default class RestaurantList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: null,
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:3000/restaurant").then((response) => {
+      response.json().then((result) => {
+        // console.log(result);
+        this.setState({ list: result });
+      });
+    });
+  }
+  render() {
+    // console.warn(this.state);
+    return (
+      <>
+        <div>
+          <h1>Restaurant List</h1>
+          {this.state.list ? (
+            <div>
+              {this.state.list.map((item, i) => (
+                <div key={i}>
+                  <span>{item.id}</span>
+                  <span>{item.name}</span>
+                  <span>{item.rating}</span>
+                  <span>{item.email}</span><br/>
+                  </div>
+              ))}
+            </div>
+          ) : (
+            <p>Please Wait...</p>
+          )}
+        </div>
+      </>
+    );
+  }
 }
-
-export default RestaurantList
